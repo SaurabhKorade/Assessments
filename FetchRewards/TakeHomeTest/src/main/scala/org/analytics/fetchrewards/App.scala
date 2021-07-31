0bddb02a6c31ae00fac4d9f7909c63c8f0f5d8f3
saurabh korade <skorade1@binghamton.edu>
2021-07-31 15:05:51 -0400

    Created and tested spark configurations

--- FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/App.scala
+++ FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/App.scala
@@ -6,6 +6,33 @@ package org.analytics.fetchrewards
  */
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    val analyticsOpt = args(0)
+    val noOfMonths = args(1).toInt
+    val outputFormat = args(2)
+
+    val appName = "AnalyticsService"
+    val sparkConfig = new SparkConfManager
+
+    val sparkSession = sparkConfig.getSparkSession(appName)
+    val sc = sparkConfig.getSparkContext(sparkSession)
+    val sqlContext = sparkConfig.getSqlContext(sparkSession)
+
+    val originalUserReceiptsDf = sqlContext.read.
+      format("csv").
+      option("header", "true").
+      option("inferSchema", "true").
+      load("C:\\Users\\saura\\Documents\\MS\\BigData\\FetchRewardsData\\rewards_receipts_lat_v3.csv")
+
+    originalUserReceiptsDf.show(false)
+
+    if (analyticsOpt.equalsIgnoreCase("analytics1")){
+
+    }
+    else if (analyticsOpt.equalsIgnoreCase("analytics2")){
+
+    }
+    else if (analyticsOpt.equalsIgnoreCase("analytics3")){
+
+    }
   }
 }
---
+++ FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/SparkConfManager.scala
@@ -0,0 +1,27 @@
+package org.analytics.fetchrewards
+
+import org.apache.spark.sql.SparkSession
+
+class SparkConfManager {
+  //create spark session
+  def getSparkSession(appName: String): SparkSession = {
+    val sparkSession = SparkSession.builder()
+      .appName(appName)
+      .master("local")
+      .getOrCreate()
+    sparkSession
+  }
+
+  //create spark context
+  def getSparkContext(sparkSession: SparkSession) = {
+    val sc = sparkSession.sparkContext
+    sc
+  }
+
+  //create sqlContext
+  def getSqlContext(sparkSession: SparkSession) = {
+    val sqlContext = sparkSession.sqlContext
+    sqlContext
+  }
+
+}
