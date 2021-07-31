38b32fdd0696edb158d70a3a154c36d5ab3ff27e
saurabh korade <skorade1@binghamton.edu>
2021-07-31 17:36:28 -0400

    Query 1 : Find best and least performing store by state

--- FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/AnalyzeData.scala
+++ FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/AnalyzeData.scala
@@ -1,12 +1,14 @@
 package org.analytics.fetchrewards
 
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{max, months_between, round, udf}
+import org.apache.spark.sql.functions.{bround, col, floor, max, months_between, round, row_number, sum, udf}
 import org.apache.spark.sql.{DataFrame, SQLContext}
 
 class AnalyzeData (sqlContext: SQLContext) {
   import sqlContext.implicits._
   def retrieveNMonthsData(noOfMonths: Int): DataFrame ={
+
+    //Reading data from local disk. Update the path to read from project resources directory.
     val originalUserReceiptsDf = sqlContext.read.
       format("csv").
       option("header", "true").
@@ -19,10 +21,16 @@ class AnalyzeData (sqlContext: SQLContext) {
       option("inferSchema", "true").
       load("C:\\Users\\saura\\Documents\\MS\\BigData\\FetchRewardsData\\rewards_receipts_item_lat_v2.csv")
 
+    //Get necessary information only
     val necessaryUserDataDF = originalUserReceiptsDf.select("RECEIPT_ID", "USER_ID", "STORE_NAME", "STORE_CITY", "STORE_STATE", "RECEIPT_PURCHASE_DATE", "RECEIPT_TOTAL", "RECEIPT_ITEM_COUNT")
     val necessaryReceiptDescDataDf = originalReceiptDescriptionDf.select("REWARDS_RECEIPT_ID", "RECEIPT_DESCRIPTION","QUANTITY", "ITEM_PRICE", "DISCOUNTED_PRICE", "BRAND", "CATEGORY")
 
+    //Compute maximum date to find the date difference. Used to segregate data of specific months
     val maxDateWindow = Window.rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+
+    //Get all the user and receipt information together for the analytics queries to run.
+    //Note: All the data is not going to be needed by all the queries. Keep for adding different queries.
+    //Fill null columns with 'Other' for naming
     val completeInfoDf = necessaryUserDataDF.
       join(necessaryReceiptDescDataDf, necessaryUserDataDF("RECEIPT_ID") === necessaryReceiptDescDataDf("REWARDS_RECEIPT_ID")).
       na.fill("Other", Seq("CATEGORY")).
@@ -31,6 +39,9 @@ class AnalyzeData (sqlContext: SQLContext) {
       withColumn("DATE_DIFF", round(months_between($"MAX_DATE", $"RECEIPT_PURCHASE_DATE"))).
       drop("REWARDS_RECEIPT_ID", "MAX_DATE")
 
+    //UDF to get sub-category from category to in-depth analysis. Get the second category.
+    //Example: Grocery|Canned & Packages => Canned & Packages
+    // Other => Other
     val getSubCategory =  (strCategory:String) => {
       val arr = strCategory.split("\\|")
       if(arr.size >= 2)
@@ -41,13 +52,37 @@ class AnalyzeData (sqlContext: SQLContext) {
 
     val getSubCategoryUDF = udf(getSubCategory)
 
+    //Get n months of data specified in the argument
     val nMonthsDataDf = completeInfoDf.
       filter(completeInfoDf("DATE_DIFF") >= noOfMonths).
       withColumn("SUB_CATEGORY", getSubCategoryUDF('CATEGORY)).
       drop("DATE_DIFF", "CATEGORY")
 
     nMonthsDataDf
-
   }
 
+  //Query 1 : Find best and least performing store by state
+  def storePerformanceByState(nMonthsDataDf: DataFrame): (DataFrame, DataFrame) ={
+    val byStoreStateDesc = Window.partitionBy("STORE_STATE").orderBy($"TOTAL_REVENUE".desc)
+    val byStoreStateAsc = Window.partitionBy("STORE_STATE").orderBy($"TOTAL_REVENUE".asc)
+
+    //Aggregate revenue for every store grouped by state to get best and least performing stores in each state
+    val stateWiseStoreRevenue = nMonthsDataDf.
+      groupBy($"STORE_STATE",$"STORE_NAME").
+      agg(sum(bround($"ITEM_PRICE")).as("TOTAL_REVENUE"))
+
+    //Get best performing stores by state
+    val bestPerformingStoreByState = stateWiseStoreRevenue.
+      withColumn("rowNum", row_number().over(byStoreStateDesc)).
+      where("rowNum == 1").drop("rowNum").
+      withColumnRenamed("STORE_NAME", "BEST_SELLING_STORE")
+
+    //Get least performing stores by state
+    val leastPerformingStoreByState = stateWiseStoreRevenue.
+      withColumn("rowNum", row_number().over(byStoreStateAsc)).
+      where("rowNum == 1").drop("rowNum").
+      withColumnRenamed("STORE_NAME", "LEAST_SELLING_STORE")
+
+    (bestPerformingStoreByState, leastPerformingStoreByState)
+  }
 }
--- FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/App.scala
+++ FetchRewards/TakeHomeTest/src/main/scala/org/analytics/fetchrewards/App.scala
@@ -2,6 +2,8 @@ package org.analytics.fetchrewards
 
 object App {
   def main(args: Array[String]): Unit = {
+
+    //Add validation for the following fields
     val analyticsOpt = args(0)
     val noOfMonths = args(1).toInt
     val outputFormat = args(2)
@@ -9,6 +11,7 @@ object App {
     val appName = "AnalyticsService"
     val sparkConfig = new SparkConfManager
 
+    //Objects to access spark configuration
     val sparkSession = sparkConfig.getSparkSession(appName)
     val sc = sparkConfig.getSparkContext(sparkSession)
     val sqlContext = sparkConfig.getSqlContext(sparkSession)
@@ -16,11 +19,13 @@ object App {
     val analyzeData = new AnalyzeData(sqlContext)
     val nMonthsData = analyzeData.retrieveNMonthsData(noOfMonths)
 
-    nMonthsData.show(false)
-    nMonthsData.printSchema()
+    //nMonthsData.show(false)
+    //nMonthsData.printSchema()
 
     if (analyticsOpt.equalsIgnoreCase("analytics1")){
-
+      val (bestPerformingStoreByState, leastPerformingStoreByState) = analyzeData.storePerformanceByState(nMonthsData)
+      bestPerformingStoreByState.show(false)
+      leastPerformingStoreByState.show(false)
     }
     else if (analyticsOpt.equalsIgnoreCase("analytics2")){
 
